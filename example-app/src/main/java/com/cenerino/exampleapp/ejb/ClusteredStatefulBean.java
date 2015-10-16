98d343d464ce8e1c5ff1795821f042c1523fb927
Marcelo Cenerino <marcelocenerine@gmail.com>
2015-10-16 22:44:08 +0100

    remove lookup code

--- example-app/src/main/java/com/cenerino/exampleapp/ejb/ClusteredStatefulBean.java
+++ example-app/src/main/java/com/cenerino/exampleapp/ejb/ClusteredStatefulBean.java
@@ -1,14 +1,22 @@
 package com.cenerino.exampleapp.ejb;
 
 import javax.ejb.Stateful;
+import javax.enterprise.context.SessionScoped;
 
 import org.jboss.ejb3.annotation.Clustered;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Stateful
 @Clustered
+@SessionScoped
 public class ClusteredStatefulBean {
 
+    private static final Logger logger = LoggerFactory.getLogger(ClusteredStatefulBean.class);
+    private int counter;
+
     public String hello() {
+        logger.info("Counter now is -> {}", ++counter);
         return "Greetings from stateful bean deployed on node " + System.getProperty("jboss.node.name");
     }
 }
--- example-app/src/main/java/com/cenerino/exampleapp/servlet/HelloServlet.java
+++ example-app/src/main/java/com/cenerino/exampleapp/servlet/HelloServlet.java
@@ -3,17 +3,11 @@ package com.cenerino.exampleapp.servlet;
 import java.io.IOException;
 
 import javax.inject.Inject;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.cenerino.exampleapp.ejb.ClusteredStatefulBean;
 import com.cenerino.exampleapp.ejb.ClusteredStatelessBean;
@@ -22,10 +16,12 @@ import com.cenerino.exampleapp.ejb.ClusteredStatelessBean;
 public class HelloServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
-    private static final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 
     @Inject
     private ClusteredStatelessBean statelessBean;
+
+    @Inject
+    private ClusteredStatefulBean statefulBean;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -38,21 +34,7 @@ public class HelloServlet extends HttpServlet {
         String from = request.getParameter("from");
 
         if (from != null && from.equals("stateful")) {
-            ClusteredStatefulBean statefull = (ClusteredStatefulBean) request.getSession().getAttribute("stateful");
-
-            if (statefull == null) {
-                logger.info("Stateful bean reference was not found in the current session");
-
-                try {
-                    Context context = new InitialContext();
-                    statefull = (ClusteredStatefulBean) context.lookup("java:module/ClusteredStatefulBean");
-                    request.getSession().setAttribute("stateful", statefull);
-                } catch (NamingException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return statefull.hello();
+            return statefulBean.hello();
         }
 
         return statelessBean.hello();
