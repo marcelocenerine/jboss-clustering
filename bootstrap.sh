bc415f6ffbb096b3d3dadc3d3d136a4c9bb5d636
Marcelo Cenerino <marcelocenerine@gmail.com>
2015-10-25 20:27:00 +0000

    create docker compose file

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,6 @@ sudo wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker vagrant
 sudo service docker restart
 
-
+# Install Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.5.0rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
